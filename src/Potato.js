0ccef3fcc95a665298d9dd1818f4375aa9dd343a
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-04-21 21:54:50 +0900

    second commit

---
+++ src/Potato.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Potato(props) {
+    console.log(props);
+    return (
+        <div>
+            <h3>Hello potato</h3>
+            <ul>
+                <span>potato list</span>
+            </ul>
+        </div>
+    );
+}
+
+export default Potato;
